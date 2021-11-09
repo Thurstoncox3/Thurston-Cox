a3016e03d0ca1b80eda4af585a0ff0af3b3ecc24
thurston cox <thurstoncox3@gmail.com>
2021-11-09 16:35:51 -0500

    updated home/intro

---
+++ thurston-cox-portfolio/src/components/Home/Introduction.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assests/bitmoji.png";
+import Tilt from "react-parallax-tilt";
+import {
+    AiFillGithub,
+    AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+// Function Home2
