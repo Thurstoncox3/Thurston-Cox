2d3a1f96e06f7c296eeff1389b369b5cb7e4610d
thurston cox <thurstoncox3@gmail.com>
2021-11-30 18:25:00 -0500

    heroku

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-// const express = require ('express');
-// const app = express();
+const express = require ('express');
+const app = express();
+const path = require('path');
 
-// const port = process.env.PORT || 3000
+const publicPath = path.join(__dirname, 'src','public');
+app.use(express.static(publicPath));
+
+const port = process.env.PORT || 3000
 
 // app.use(express.static(__dirname + '/src'));
-// app.get('/', function(req, res){
-//     res.render('index');
-// });
+app.get('/', function(req, res){
+    res.sendFile(path.join(publicPath,'index.html'));
+});
 
-// app.listen(port, function(){
-//     console.log('App Running');
-// });
+app.listen(port, function(){
+    console.log(`App Running at ${port}`);
+});
