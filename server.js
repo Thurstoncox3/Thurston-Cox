fc3fb58b36f771cc5e558452bbaacb6474605add
thurston cox <thurstoncox3@gmail.com>
2021-11-30 16:10:12 -0500

    heroku staging

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 
 const port = process.env.PORT || 3000
 
-app.use(express.static(__dirname + '/public' + '/src'));
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
-    res.render('index');
+    res.render('/src');
 });
 
 app.listen(port, function(){
