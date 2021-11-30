aa110c1563e21523672bac12b335f0d44536db9a
thurston cox <thurstoncox3@gmail.com>
2021-11-30 17:26:46 -0500

    heroku

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require ('express');
-const app = express();
+// const express = require ('express');
+// const app = express();
 
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
 
-app.use(express.static(__dirname + '/src'));
-app.get('/', function(req, res){
-    res.render('index');
-});
+// app.use(express.static(__dirname + '/src'));
+// app.get('/', function(req, res){
+//     res.render('index');
+// });
 
-app.listen(port, function(){
-    console.log('App Running');
-});
+// app.listen(port, function(){
+//     console.log('App Running');
+// });
